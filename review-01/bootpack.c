692aac7d8fb92ab145a04b2f9fc16f9bf1a01469
Dreamer <365373011@qq.com>
2023-05-23 08:51:09 +0800

    界面展示 字体输出

--- review-01/bootpack.c
+++ review-01/bootpack.c
@@ -7,11 +7,28 @@ void io_out8(int port, int data);
 int io_store_eflags(int eflags);
 int io_load_eflags();
 
+struct Boot_info
+{
+	// 引导扇区设置
+	char cyls;
+	// LED 指示灯状态
+	char leds;
+	// 关于颜色的信息
+	char vmode;
+	// 分辨率X
+	short scrnx;
+	// 分辨率y
+	short scrny;
+	// 图像缓冲区的起始地址
+	char *vram;
+};
+
+
 void init_palette(void)
 {
 	// vag调色盘最多只能设置16种不同的颜色
 	static unsigned char table_rgb[18 * 3] = {
-		0x00, 0x00, 0x00, /*  0:黑 */
+	0x00, 0x00, 0x00, /*  0:黑 */
 		0xff, 0x00, 0x00, /*  1:梁红 */
 		0x00, 0xff, 0x00, /*  2:亮绿 */
 		0xff, 0xff, 0x00, /*  3:亮黄 */
@@ -20,7 +37,7 @@ void init_palette(void)
 		0x00, 0xff, 0xff, /*  6:浅亮蓝 */
 		0xff, 0xff, 0xff, /*  7:白 */
 		0xc6, 0xc6, 0xc6, /*  8:亮灰 */
-		0x84, 0x00, 0x00,	/*  9:暗红 */
+		// 0x84, 0x00, 0x00,	/*  9:暗红 */
 		0x00, 0x84, 0x00, /* 10:暗绿 */
 		0x84, 0x84, 0x00, /* 11:暗黄 */
 		0x00, 0x00, 0x84, /* 12:暗青 */
@@ -29,15 +46,13 @@ void init_palette(void)
 		0x84, 0x84, 0x84, /* 15:暗灰 */
 		0xb6, 0xa3, 0xbc, /* 16:dreamer鬃毛颜色 */
 
-        0xFF, 0xC0, 0xCB,
-
 	};
 
-    set_palette(0, 18, table_rgb);
-    return;
+	set_palette(0, 18, table_rgb);
+	return;
 }
 
-void set_palette(int start, int end, unsigned char *rgb) 
+void set_palette(int start, int end, unsigned char *rgb)
 {
 	int i, eflags;
 	// 记录中断寄存器中的值
@@ -62,32 +77,106 @@ void set_palette(int start, int end, unsigned char *rgb)
 	return;
 }
 
-
-void boxfill8(unsigned char *vram, int xSize, unsigned int color, int x0, int y0, int x1, int y1) {
+void boxfill8(unsigned char *vram, int xSize, int x0, int y0, int x1, int y1, unsigned char color)
+{
 	int x, y;
 
-	for (x = x0; x <= x1; x++) {
-		for (y = y0; y <= y1; y++) {
+	for (x = x0; x <= x1; x++)
+	{
+		for (y = y0; y <= y1; y++)
+		{
 			*(vram + xSize * y + x) = color;
 		}
 	}
 }
 
+static char font_A[16] = {
+	0x00, 0x18, 0x18, 0x18, 0x18, 0x24, 0x24, 0x24,
+	0x24, 0x7e, 0x42, 0x42, 0x42, 0xe7, 0x00, 0x00};
+
+/**
+ * varam: 显存地址
+ * xSize: 每行像素数 320 * 200就是320
+ * c: 颜色下标
+ * x: 字符串显示x轴 从左到右空出多少个像素
+ * y: 字符显示y轴   
+ * font: 字符序列
+*/
+void printFont8(unsigned char *vram, int x_size, char c, int x, int y, char font[16])
+{
+	int i;
+	char *p, d /* data */;
+	for (i = 0; i < 16; i++)
+	{
+		// 和矩形计算公式一样 根据行数和列数 计算出字符需要展示的位置
+		p = vram + (y + i) * x_size + x;
+		d = font[i];
+		
+		if ((d & 0x80) != 0)
+		{
+			p[0] = c;
+		}
+		if ((d & 0x40) != 0)
+		{
+			p[1] = c;
+		}
+		if ((d & 0x20) != 0)
+		{
+			p[2] = c;
+		}
+		if ((d & 0x10) != 0)
+		{
+			p[3] = c;
+		}
+		if ((d & 0x08) != 0)
+		{
+			p[4] = c;
+		}
+		if ((d & 0x04) != 0)
+		{
+			p[5] = c;
+		}
+		if ((d & 0x02) != 0)
+		{
+			p[6] = c;
+		}
+		if ((d & 0x01) != 0)
+		{
+			p[7] = c;
+		}
+	}
+	return;
+}
+
+void init_screen(char *wram, int x_size, int y_size)
+{
+
+	boxfill8(wram,x_size, 0, 0, x_size, 180, 0x0f);
+	boxfill8(wram,x_size, 0, y_size - 20, x_size, 200, 0x08);
+	boxfill8(wram,x_size, 0, y_size - 19, x_size, 182, 0x07);
+	boxfill8(wram,x_size, 2, y_size - 15, x_size - 280, 186, 0x07);
+	boxfill8(wram,x_size, 3, y_size - 6, x_size - 280, 195, 0x0e);
+	boxfill8(wram,x_size, 2, y_size - 15, x_size - 317, 195, 0x07);
+	boxfill8(wram,x_size, 40, y_size - 15, x_size - 279, 196, 0x00);
+	boxfill8(wram,x_size, 2, y_size - 5, 40, x_size - 124, 0x00);
+	// boxfill8(wram, 40, y_size - 6, 40, x_size - 123, 0x05);
+}
+
+
 void HariMain(void)
 {
-    int j = 0;
-    int i = 0xa0000;
-    init_palette();
+	struct Boot_info *boot_info = (struct Boot_info *)0x0ff0;
 
-	char *p;
+	init_palette();
 
-	p = (char *) 0xa0000;
+	init_screen(boot_info->vram, boot_info->scrnx, boot_info->scrny);
 
-	boxfill8(i, 320, 0xf, 70, 150, 120, 172);
+
+	// printFont8(boot_info->vram, boot_info->scrnx, 0x07, 8, 8, hankaku + 'B' * 16);
 
 
 fin:
-    io_hlt();
+	io_hlt();
 
-    goto fin;
+	goto fin;
 }
